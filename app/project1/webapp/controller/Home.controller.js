0153b62c1b2b58afd066ea4d8045c1cba46fa2bc
102393AmolShrivastava <amoshrivastava@deloitte.com>
2024-07-14 19:45:35 +0530

    changes made to get data from backend to frontend

--- app/project1/webapp/controller/Home.controller.js
+++ app/project1/webapp/controller/Home.controller.js
@@ -1,12 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    
 ],
 function (Controller) {
     "use strict";
 
     return Controller.extend("com.tcs.hr.ui.project1.controller.Home", {
         onInit: function () {
-            console.log('inside home controller')
+            console.log('inside home controller');
+            debugger;
         }
     });
 });
